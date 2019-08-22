2f5035eae191b5a9897f6faad08987115976b908
Rayane BEN-HMIDANE <rayane.benhmidane32@hotmail.fr>
2019-08-22 12:32:37 +0200

    Add Scripts + Algorithm explanation

---
+++ Scripts/whitelist_generator.py
@@ -0,0 +1,40 @@
+"""
+    instabot example
+    Whitelist generator: generates a list of users which
+    will not be unfollowed.
+"""
+
+import sys
+import os
+import random
+
+sys.path.append(os.path.join(sys.path[0], '../../'))
+from instabot import Bot
+
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-u', type=str, help="username")
+parser.add_argument('-p', type=str, help="password")
+
+
+
+bot = Bot()
+bot.login(username=args.u, password=args.p, use_cookie=True)
+
+print("This script will generate whitelist.txt file with users"
+      "who will not be unfollowed by bot. "
+      "Press Y to add user to whitelist. Ctrl + C to exit.")
+your_following = bot.following
+already_whitelisted = bot.read_list_from_file("whitelist.txt")
+rest_users = list(set(your_following) - set(already_whitelisted))
+random.shuffle(rest_users)
+with open("whitelist.txt", "a") as f:
+    for user_id in rest_users:
+        user_info = bot.get_user_info(user_id)
+        print(user_info["username"])
+        print(user_info["full_name"])
+
+        input_line = sys.stdin.readline().lower()
+        if "y" in input_line.lower():
+            f.write(str(user_id) + "\n")
+            print("ADDED.\r")
